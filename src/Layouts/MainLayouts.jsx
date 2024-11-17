f201862826015a486ca76e6d5951091b649d5ff8
Jihanchy <habibullahalquaderi2005@gmail.com>
2024-11-18 00:13:09 +0600

    added navbar

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../Components/Navbar';
 
 const MainLayouts = () => {
     return (
-        <div>
+        <div className='font-poppins'>
             {/* navbar */}
+            <section className='bg-slate-100 sticky top-0'>
+            <Navbar></Navbar>
+            </section>
             {/* dynamic section */}
+            <section>
             <Outlet></Outlet>
+            </section>
             {/* footer */}
         </div>
     );
--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -1,12 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../Layouts/MainLayouts";
 import ErrorPage from "../Pages/ErrorPage";
+import Home from "../Pages/Home";
 
 const router = createBrowserRouter([
     {
         path:'/',
         errorElement: <ErrorPage></ErrorPage> ,
-        element:<MainLayouts></MainLayouts>
+        element:<MainLayouts></MainLayouts>,
+        children: [
+            {
+                path: '/',
+                element:<Home></Home>
+            },
+            {
+                path:'/profile',
+                element: <h2>Profile</h2>
+            }
+        ]
     }
 ])
 
